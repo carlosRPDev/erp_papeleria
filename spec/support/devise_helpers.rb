f0eaa0bc9f5db45d32c86d608b5a0d3f06d601f6
Carlos Rodriguez <ucroxlive@gmail.com>
2025-11-20 20:13:22 -0500

    engine erp_core refactor

---
+++ spec/support/devise_helpers.rb
@@ -0,0 +1,15 @@
+module DeviseRequestSpecHelpers
+  def sign_in(user)
+    post erp_core.user_session_path, params: {
+      user: { email: user.email, password: user.password }
+    }
+  end
+
+  def sign_out
+    delete erp_core.destroy_user_session_path
+  end
+end
+
+RSpec.configure do |config|
+  config.include DeviseRequestSpecHelpers, type: :request
+end
